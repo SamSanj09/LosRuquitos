9aa2986f62a9e2a535216aa5f9aeda396c72f35f
SamSanj09 <jonathan.sanjines@ucb.edu.bo>
2024-04-10 15:18:38 -0400

    arreglado

--- contactos.php
+++ contactos.php
@@ -13,21 +13,24 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 } 
+// Recuperar datos del formulario solo si se han enviado
+if(isset($_POST['nombre'], $_POST['celular'], $_POST['correo'], $_POST['dmensaje'])) {
+    $nombre = $_POST['nombre'];
+    $celular = $_POST['celular'];
+    $correo = $_POST['correo'];
+    $dmensaje = $_POST['dmensaje'];
 
-// Recuperar datos del formulario
-$nombre = $_POST['nombre'];
-$celular = $_POST['celular'];
-$correo = $_POST['correo'];
-$dmensaje = $_POST['dmensaje'];
+    // Insertar datos en la base de datos
+    $sql = "INSERT INTO contactos(nombre, celular, correo, dmensaje) VALUES ('$nombre', '$celular', '$correo', '$dmensaje')";
 
-// Insertar datos en la base de datos
-$sql = "INSERT INTO contactos(nombre, celular, correo, dmensaje) VALUES ('$nombre', '$celular', '$correo', '$dmensaje')";
-
-if ($conn->query($sql) === TRUE) {
-    echo '<script>alert("Los datos se han insertado correctamente.");</script>';
-} else {
-     echo '<script>alert("Error al insertar los datos.");</script>';
+    if ($conn->query($sql) === TRUE) {
+        echo '<script>alert("Los datos se han insertado correctamente.");</script>';
+    } else {
+        echo '<script>alert("Error al insertar los datos.");</script>';
+    }
 }
+
+
 
 // Cerrar conexión a la base de datos
 $conn->close();
